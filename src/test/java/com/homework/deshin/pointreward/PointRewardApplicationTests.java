f89fc13e53d3f6a2b0ca8696e9bb180f133f8b32
berryberry189 <berryberry189@naver.com>
2023-01-30 15:03:16 +0900

    Initial commit

---
+++ src/test/java/com/homework/deshin/pointreward/PointRewardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.deshin.pointreward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointRewardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
