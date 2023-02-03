c3ccca4f7ca9f9f9932c465d6172fca033542415
berryberry189 <berryberry189@naver.com>
2023-02-04 01:27:22 +0900

    Test : 포인트 계산 테스트 케이스 작성

---
+++ src/test/java/com/homework/deshin/pointreward/service/PointCalculatorTest.java
@@ -0,0 +1,43 @@
+package com.homework.deshin.pointreward.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+import com.homework.deshin.pointreward.domain.PointReward;
+import com.homework.deshin.pointreward.repository.PointRewardRepository;
+import java.time.LocalDate;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest(classes = PointCalculator.class)
+class PointCalculatorTest {
+
+  @Autowired
+  PointCalculator pointCalculator;
+
+  @MockBean
+  PointRewardRepository pointRewardRepository;
+
+  private LocalDate today = LocalDate.now();
+
+  @Test
+  void 전날에_출석한_이력있으면_전날포인트_더하기_100포인트_제공() {
+    String memberId = "member_1";
+
+    given(pointRewardRepository.findByMemberIdAndPayAtGreaterThanEqualAndPayAtLessThan(
+        memberId, today.minusDays(1).atStartOfDay(), today.atStartOfDay()))
+        .willReturn(Optional.of(PointReward.builder()
+            .point(100)
+            .memberId(memberId)
+            .payAt(today.minusDays(1).atStartOfDay())
+            .build()));
+
+    int point = pointCalculator.calculate(memberId, today);
+
+    assertEquals(200, point);
+  }
+
+}
