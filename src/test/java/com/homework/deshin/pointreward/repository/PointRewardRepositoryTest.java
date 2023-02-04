b79443b9b6752bf83030426aedbb9033a5285381
berryberry189 <berryberry189@naver.com>
2023-02-05 00:17:13 +0900

    Test : 날짜별 포인트 지급 목록 테스트 수정

--- src/test/java/com/homework/deshin/pointreward/repository/PointRewardRepositoryTest.java
+++ src/test/java/com/homework/deshin/pointreward/repository/PointRewardRepositoryTest.java
@@ -1,13 +1,11 @@
 package com.homework.deshin.pointreward.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.homework.deshin.pointreward.domain.PointReward;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -50,12 +48,11 @@ class PointRewardRepositoryTest {
   @Test
   @DisplayName("날짜별 포인트 지급 목록 테스트")
   void findByMemberIdAndPayAtGreaterThanEqualAndPayAtLessThan(){
-
     String memberId = "member_1";
-    Optional<PointReward> pointRewardOpt = pointRewardRepository.findByMemberIdAndPayAtGreaterThanEqualAndPayAtLessThan(memberId,
-        LocalDate.now().minusDays(1).atStartOfDay(), LocalDate.now().atStartOfDay());
+    PointReward pointReward = pointRewardRepository.findByMemberIdAndPayAtGreaterThanEqualAndPayAtLessThan(memberId,
+        LocalDate.now().minusDays(1).atStartOfDay(), LocalDate.now().atStartOfDay()).orElseThrow();
 
-    assertTrue(pointRewardOpt.isEmpty());
+    assertEquals(memberId, pointReward.getMemberId());
   }
 
 }
