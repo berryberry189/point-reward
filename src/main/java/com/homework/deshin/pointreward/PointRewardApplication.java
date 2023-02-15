b69c630ca0adf2461d42cda671fb91e0f2f6e110
berryberry189 <berryberry189@naver.com>
2023-02-15 15:16:19 +0900

    Refactor : 사용하지 않는 어노테이션 삭제

--- src/main/java/com/homework/deshin/pointreward/PointRewardApplication.java
+++ src/main/java/com/homework/deshin/pointreward/PointRewardApplication.java
@@ -3,10 +3,8 @@ package com.homework.deshin.pointreward;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
-@EnableScheduling
 @SpringBootApplication
 public class PointRewardApplication {
 
